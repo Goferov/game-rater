30419ac081b28e8908737c35a8b9df06c0da2281
Goferov <marcingodfryd@gmail.com>
2024-05-20 23:32:00 +0200

    Create GameController and ShowAddress, add methods to UserController

---
+++ app/Http/Controllers/User/ShowAddress.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Faker\Factory;
+
+class ShowAddress extends Controller
+{
+    public function __invoke()
+    {
+        $faker = Factory::create();
+        $address = [
+            'postalCode' => $faker->postcode,
+            'street' => $faker->streetAddress,
+            'houseNumber' => $faker->numberBetween(1, 100),
+            'flatNumber' => $faker->numberBetween(1, 100),
+        ];
+
+        return view('user.address', [
+            'address' => $address,
+        ]);
+    }
+}
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class UserController extends Controller
 {
-    public function list()
+    public function list(Request $request)
     {
         return view('user.list');
+    }
+
+    public function show(int $userId)
+    {
+        return view('user.show', [
+            'userId' => $userId
+        ]);
     }
 }
