0cd00d0a1cc63a90539f4d814729febebc8cf1b3
Goferov <marcingodfryd@gmail.com>
2024-05-25 14:49:33 +0200

    Implement laravel ui with laravel mix

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -3,6 +3,8 @@
         <title>
             @yield('title', $applicationName)
         </title>
+        <script src="{{ mix('js/app.js') }}"></script>
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
     <body>
         <h1>{{ $applicationName }}</h1>
