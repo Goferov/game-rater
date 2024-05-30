cea371bdaa30844e52b7e3e09b93fca8aaff81f5
Goferov <marcingodfryd@gmail.com>
2024-05-30 19:39:30 +0200

    Create models and implement eloquent in controllers

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Game;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
@@ -13,12 +14,7 @@ class GameController extends Controller
      */
     public function index(): View
     {
-        $games = DB::table('games')
-            ->join('genres', 'games.genre_id', '=', 'genres.id')
-            ->select(
-                'games.id', 'games.title', 'games.score',
-                'genres.id as genre_id', 'genres.name as genre_name'
-            )
+        $games = Game::with('genre')
             ->paginate(10);
 
         return view('game.list', [
@@ -28,25 +24,19 @@ class GameController extends Controller
 
     public function dashboard(): View
     {
-        $bestGames = DB::table('games')
-            ->join('genres', 'games.genre_id', '=', 'genres.id')
-            ->select(
-                'games.id', 'games.title', 'games.score',
-                'genres.id as genre_id', 'genres.name as genre_name'
-            )
-            ->where('score', '>', 9)
-            ->get();
+        $bestGames = Game::best()->get();
 
         $stats = [
-            'count' => DB::table('games')->count(),
-            'countScoreGtSeven' => DB::table('games')->where('score', '>', 7)->count(),
-            'max' => DB::table('games')->max('score'),
-            'min'=> DB::table('games')->min('score'),
-            'avg'=> DB::table('games')->avg('score'),
+            'count' => Game::count(),
+            'countScoreGtSeven' => Game::where('score', '>', 7)->count(),
+            'max' => Game::max('score'),
+            'min'=> Game::min('score'),
+            'avg'=> Game::avg('score'),
         ];
 
-        $scoreStats = DB::table('games')
-            ->select(DB::raw('count(*) as count'), 'score')
+        $scoreStats = Game::select(
+            Game::raw('count(*) as count'), 'score'
+        )
             ->having('count', '>', 10)
             ->groupBy('score')
             ->orderBy('count', 'desc')
@@ -59,55 +49,12 @@ class GameController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(int $gameId): View
     {
-        $game = DB::table('games')->find($gameId);
+        $game = Game::find($gameId);
 
         return view('game.show', [
             'game' => $game
         ]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(int $gameId)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, int $gameId)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(int $gameId)
-    {
-        //
     }
 }
---
+++ app/Models/Game.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+
+class Game extends Model
+{
+    protected $fillable = [
+        'title', 'description', 'score', 'publisher', 'genre_id'
+    ];
+
+    protected $attributes = [
+        'score' => 5
+    ];
+
+    public function genre()
+    {
+        return $this->belongsTo(Genre::class);
+    }
+
+    public function scopeBest(Builder $query): Builder
+    {
+        return $query
+            ->with('genre')
+            ->where('score', '>=', 9)
+            ->orderBy('score', 'desc')
+        ;
+    }
+
+    public function scopeGenre(Builder $query, int $genreId): Builder
+    {
+        return $query->where('genre_id', $genreId);
+    }
+
+    public function scopePublisher(Builder $query, string $name): Builder
+    {
+        return $query->where('publisher', $name);
+    }
+}
