e53c6e629a6599645b7978b6130846f44322be51
Goferov <marcingodfryd@gmail.com>
2024-05-31 13:59:41 +0200

    Create support for registration, login and password reset

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\GameController;
 use App\Http\Controllers\Home\MainPageController;
 use App\Http\Controllers\User\ShowAddressController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
@@ -17,24 +18,31 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/', MainPageController::class)
-    ->name('home.mainPage');
+Route::group(['middleware' => ['auth']], function (){
+    Route::get('/', MainPageController::class)
+        ->name('home.mainPage');
 
-Route::get('users', [UserController::class, 'list'])
-    ->name('get.users');
+    Route::get('users', [UserController::class, 'list'])
+        ->name('get.users');
 
-Route::get('users/{userId}', [UserController::class, 'show'])
-    ->name('get.user.show');
+    Route::get('users/{userId}', [UserController::class, 'show'])
+        ->name('get.user.show');
 
-Route::get('users/{id}/address', ShowAddressController::class)
-    ->where(['id' => '[0-9]+'])
-    ->name('get.users.address');
+    Route::get('users/{id}/address', ShowAddressController::class)
+        ->where(['id' => '[0-9]+'])
+        ->name('get.users.address');
 
-Route::get('games/dashboard', [GameController::class, 'dashboard'])
-    ->name('games.dashboard');
+    Route::get('games/dashboard', [GameController::class, 'dashboard'])
+        ->name('games.dashboard');
 
-Route::get('games', [GameController::class, 'index'])
-    ->name('games.index');
+    Route::get('games', [GameController::class, 'index'])
+        ->name('games.index');
 
-Route::get('games/{game}', [GameController::class, 'show'])
-    ->name('games.show');
+    Route::get('games/{game}', [GameController::class, 'show'])
+        ->name('games.show');
+});
+
+
+
+Auth::routes();
+
